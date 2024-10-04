b157ab6b4a57ebf43d51766b5e70600cea892eaf
Ayan Biswas <ayanvaai@gmail.com>
2024-10-04 21:51:10 +0530

    Updates

---
+++ precache-manifest.f0aeddd05167acfec3ceac927b4436ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07854ee6eee9307c6595a6845c805d25",
+    "url": "/Snapshot/index.html"
+  },
+  {
+    "revision": "5970a17890755c05c322",
+    "url": "/Snapshot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/Snapshot/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Snapshot/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5970a17890755c05c322",
+    "url": "/Snapshot/static/js/main.e5f394ab.chunk.js"
+  },
+  {
+    "revision": "0cb478cdef5829aa87dd",
+    "url": "/Snapshot/static/js/runtime-main.014347dc.js"
+  }
+]);
